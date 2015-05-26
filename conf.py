f1d7e2f0f22a2467bd985191e7a9023e7e9db5f3
fomightez <wayne4.antispam@server.fake>
2015-05-26 11:40:26 -0400

    added conf.py to see if can get copyright to work

---
+++ conf.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Wayne's documentation build configuration file
+#
+
+
+import sys, os
+
+# If building on readthedocs.org, you can use the following test:
+# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+
+
+
+# -- General configuration -----------------------------------------------------
+
+
+# General information about the project.
+#project = u'Waye's stuff
+copyright = u'2015 Wayne Decatur'
